57238b5c97783871afe587f2a9b13644dc19bf36
ga2006206521 <ga2006206521@gmail.com>
2022-06-30 18:33:07 +0800

    feat / 好玩遊戲

--- index.js
+++ index.js
@@ -3,59 +3,65 @@ var yourScore = 0;
 var opponent;
 var opponentScore = 0;
 
+var choices = ["r", "p", "s"];
 
-var choices = ["r", "p", "s"]
-
-window.onload = function() {
-    for (let i = 0; i < 3; i++) {
-        // 新增<img id="rock" src="r.png">
-        let choice = document.createElement('img')
-        choice.id = choices[i];
-        choice.src = choices[i] + ".png";
-        // 新增點擊事件
-        choice.addEventListener('click', selectChoice);
-        document.getElementById("choices").append(choice)
+window.onload = function () {
+  for (let i = 0; i < 3; i++) {
+    // 新增<img id="rock" src="r.png">
+    let choice = document.createElement("img");
+    choice.id = choices[i];
+    choice.src = choices[i] + ".png";
+    // 新增點擊事件
+    choice.addEventListener("click", selectChoice);
+    document.getElementById("choices").append(choice);
+  }
+  document.addEventListener("click", function test(e) {
+    console.log(e.target);
+    let imgId = e.target;
+    console.log(imgId);
+    if (imgId == r) {
+      imgId.src = "/rock.png";
+    } else if (imgId == p) {
+      imgId.src = "/cloth.png";
+    } else if (imgId == s) {
+      imgId.src = "/shears.png";
     }
-}
+  });
+};
 
 function selectChoice() {
-    you = this.id;
-    document.getElementById("my-choice").src = you + ".png"
+  you = this.id;
+  document.getElementById("my-choice").src = you + ".png";
 
-    // 新增電腦隨機出
+  // 新增電腦隨機出
 
-    opponent = choices[Math.floor(Math.random() * 3)];
-    document.getElementById('choice').src = opponent + ".png"
+  opponent = choices[Math.floor(Math.random() * 3)];
+  document.getElementById("choice").src = opponent + ".png";
 
-    if(you == opponent) {
-        yourScore += 0;
-        opponentScore += 0;
-    }else {
-        if(you == "r") {
-            if(opponent == "s") {
-                yourScore += 1;
-            }
-            else if(opponent = "p") {
-                opponentScore += 1 ;
-            }
-        }
-        else if(you == "s") {
-            if(opponent == "p") {
-                yourScore += 1;
-            }
-            else if(opponent = "r") {
-                opponentScore += 1 ;
-            }
-        }
-        else if(you == "p") {
-            if(opponent == "r") {
-                yourScore += 1;
-            }
-            else if(opponent = "s") {
-                opponentScore += 1 ;
-            }
-        }
+  if (you == opponent) {
+    yourScore += 0;
+    opponentScore += 0;
+  } else {
+    if (you == "r") {
+      if (opponent == "s") {
+        yourScore += 1;
+      } else if ((opponent = "p")) {
+        opponentScore += 1;
+      }
+    } else if (you == "s") {
+      if (opponent == "p") {
+        yourScore += 1;
+      } else if ((opponent = "r")) {
+        opponentScore += 1;
+      }
+    } else if (you == "p") {
+      if (opponent == "r") {
+        yourScore += 1;
+      } else if ((opponent = "s")) {
+        opponentScore += 1;
+      }
     }
-    document.getElementById('my-score').innerText = yourScore;
-    document.getElementById('score').innerText = opponentScore
+  }
+  document.getElementById("my-score").innerText = yourScore;
+  document.getElementById("score").innerText = opponentScore;
 }
